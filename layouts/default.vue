05297e0dad15a634eb4c1ed7cfbec5064cac0e40
Kolapo Oni <kolapooni@Kolapos-MacBook-Pro.local>
2020-10-17 13:33:46 +0100

    Add home page layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="c-app">
-    <div class="c-nav">
+    <nav class="c-nav">
       <div class="c-nav__logo">
         <img src="~/assets/images/flag.svg" alt="logo" />
         <span>NG Voices</span>
       </div>
-      <div class="c-nav__dropdown">
+      <div class="c-dropdown">
         <select id="" name="">
           <option value="">Hausa</option>
           <option value="">Efik</option>
           <option value="">Nupe</option>
         </select>
-        <img src="~/assets/images/icon-dropdown.svg" alt="" />
+        <svg
+          width="14"
+          height="10"
+          viewBox="0 0 14 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 2C3.36846 3.94705 5.13246 5.69884 6.44444 7.65887C7.18643 8.76735 8.93806 6.86489 9.47475 6.33847C10.1826 5.64418 12 3.54808 12 2.75452"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </div>
       <a href="https://endsars.com/" target="_blank" class="c-button">
         <span>endsars.com</span>
@@ -19,7 +33,7 @@
           <img src="~/assets/images/icon-link.svg" alt="" />
         </div>
       </a>
-    </div>
+    </nav>
     <Nuxt />
   </div>
 </template>
@@ -32,7 +46,7 @@
 }
 
 .c-nav {
-  position: fixed;
+  position: absolute;
   padding: 30px 0px;
   font-size: 1.6rem;
   width: $container-width-default;
@@ -40,6 +54,7 @@
   display: flex;
   align-items: center;
   justify-content: space-between;
+  top: 0;
 
   &__logo {
     display: flex;
@@ -47,28 +62,6 @@
 
     img {
       margin-right: 10px;
-    }
-  }
-
-  &__dropdown {
-    position: relative;
-    display: flex;
-    align-items: center;
-
-    select {
-      width: 500px;
-      font-size: 1.8rem;
-      padding: 12px 0px;
-      text-align-last: center;
-      border-radius: 10px;
-      border: solid 2px black;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-    }
-
-    img {
-      position: absolute;
-      right: 20px;
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="c-home">
-    <div></div>
+    <header>
+      <h4>Spread awareness about</h4>
+      <h4>#ENDSARS to people in their</h4>
+      <h4>native languages.</h4>
+    </header>
+    <div class="c-dropdown">
+      <select id="" name="">
+        <option disabled selected>Select a language to start</option>
+        <option value="efik">Efik</option>
+        <option value="nupe">Nupe</option>
+      </select>
+      <svg
+        width="14"
+        height="10"
+        viewBox="0 0 14 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2 2C3.36846 3.94705 5.13246 5.69884 6.44444 7.65887C7.18643 8.76735 8.93806 6.86489 9.47475 6.33847C10.1826 5.64418 12 3.54808 12 2.75452"
+          stroke="white"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+    <p>
+      Select a language and get voice notes, broadcast messages and posters in
+      that language
+    </p>
   </div>
 </template>
 
@@ -10,7 +40,33 @@ export default {}
 
 <style lang="scss" scoped>
 .c-home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
-  font-size: 1.6rem;
+  margin-top: 30vh;
+  text-align: center;
+
+  header {
+    font-size: 3.2rem;
+    line-height: 42px;
+    font-weight: 700;
+    margin-bottom: 50px;
+  }
+
+  select {
+    background: black;
+    color: white;
+    font-size: 1.8rem;
+    padding: 16px 0px;
+  }
+
+  & > :last-child {
+    margin-top: 50px;
+    font-size: 1.4rem;
+    width: 100%;
+    max-width: 395px;
+    line-height: 21px;
+  }
 }
 </style>
