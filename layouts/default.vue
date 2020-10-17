468a17275285313ce799e36d501225837ff27711
onyekvchi <onyekachi.mbaike@gmail.com>
2020-10-17 05:30:44 +0100

    Add structure for /language#posters

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,30 +6,5 @@
 </template>
 
 <style lang="scss">
-html {
-  font-family: 'Inter', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-weight: normal;
-  font-size: 62.5%;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.c-app {
-  width: 90vw;
-  margin: 0 auto;
-}
-
-.c-nav {
-  width: 100%;
-}
+@import '~/assets/scss/general/base.scss';
 </style>
--- pages/_language.vue
+++ pages/_language.vue
@@ -1,9 +1,82 @@
 <template>
-  <div class="c-language"></div>
+  <div class="container">
+    <div class="c-section c-section--posters">
+      <h2 class="c-section__title">Posters</h2>
+      <div class="c-posters">
+        <div class="c-poster">
+          <div class="c-poster__image u-radius-md"></div>
+          <div class="c-poster__actions">
+            <button class="button">
+              <span class="button__text">Download</span>
+              <span class="button__icon"></span>
+            </button>
+          </div>
+        </div>
+        <div class="c-poster">
+          <div class="c-poster__image u-radius-md"></div>
+          <div class="c-poster__actions">
+            <button class="button">
+              <span class="button__text">Download</span>
+              <span class="button__icon"></span>
+            </button>
+          </div>
+        </div>
+        <div class="c-poster">
+          <div class="c-poster__image u-radius-md"></div>
+          <div class="c-poster__actions">
+            <button class="button">
+              <span class="button__text">Download</span>
+              <span class="button__icon"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Language',
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.c-posters {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+}
+
+.c-poster {
+  width: 32%;
+  &__image {
+    height: 300px;
+    margin-bottom: 15px;
+    background-color: $color-gray;
+  }
+}
+
+.button {
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  background: transparent;
+  cursor: pointer;
+
+  &__text {
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  &__icon {
+    margin-left: 10px;
+    height: 16px;
+    width: 16px;
+    background: $color-yellow;
+    border-radius: 100%;
+  }
+}
+</style>
